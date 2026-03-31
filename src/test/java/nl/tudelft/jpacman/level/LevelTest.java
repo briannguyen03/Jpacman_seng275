a1829f439738a993174dad7d0352bee61351d6fb
brian nguyen <nguyen.brian1430@gmail.com>
2026-03-31 13:19:11 -0700

    init LevelTest

---
+++ src/test/java/nl/tudelft/jpacman/level/LevelTest.java
@@ -0,0 +1,51 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.board.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelTest {
+    // public Level(Board board, List<Ghost> ghosts, List<Square> startPositions,
+    //                 CollisionMap collisionMap) {
+
+    Board testBoard;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void registerPlayer() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void isInProgress() {
+    }
+
+    @Test
+    void isAnyPlayerAlive() {
+    }
+
+    @Test
+    void remainingPellets() {
+    }
+}
